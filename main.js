78483b52b248428c5ab243b040ae196e9fed8282
Gulnoza Muminova <gmcharmy@gmail.com>
2020-05-27 15:58:20 -0400

    CRUD

--- main.js
+++ main.js
@@ -3,15 +3,74 @@ var config = require('./config.json');
 var util = require('util')
 
 var pool = mysql.createPool({
-host : config.dbhost,
-user : config.dbuser,
-password : config.dbpassword,
-database : config.dbname
+    host: config.dbhost,
+    user: config.dbuser,
+    password: config.dbpassword,
+    database: config.dbname
 });
+
 pool.query = util.promisify(pool.query);
 
-exports.hendler = async (event) => {
-    
-    var result = await pool.query('select * from events;')
+exports.handler = async (e) => {
+    let action = e.httpRequest;
+    let data = [];
+    let result;
+
+    if (action === 'GET') {
+        if ('event_id' in e.body) {
+            data = await getOneEvent(e.body.event_id);
+            data = data[0];
+
+        }
+        else {
+            data = await getAllEvents();
+        }
+    }
+    else if (action === 'POST') {
+        data = await createEvent(e.body);
+    }
+    else if (action === 'PUT') {
+        data = await updateEvent(e.body);
+    }
+    else if (action === 'DELETE') {
+        data = await deleteEvent(e.body.event_id);
+    }
+
+    result = {
+        status: 200,
+        message: 'success',
+        data: data
+    };
+
     return result;
+};
+
+const getAllEvents = () => {
+    return pool.query('select * from events;');
+};
+
+const getOneEvent = async (id) => {
+    let result = await pool.query(`select * from events where event_id = ${id};`);
+    return result;
+};
+
+const createEvent = async (params) => {
+    const { organizer, venue, date } = params;
+    let result = await pool.query(`insert into events (organizer, venue, date) values ("${organizer}", "${venue}", "${date}");`);
+    let createdEvent = getOneEvent(result.insertId);
+    return createdEvent;
+};
+
+const deleteEvent = async (id) => {
+    let result = await pool.query(`delete from events where event_id = ${id};`);
+    if (result.affectedRows === 0) {
+        return { errorMessage: "Unprocessed." };
+    }
+    return result;
+};
+const updateEvent = async (params) => {
+    const { event_id, organizer, venue, date } = params;
+    let result = await pool.query(`update events organizer = ${organizer}, ${venue}, ${date}  $ in events where event_id = ${event_id};`);
+    let updatedEvent = await getOneEvent(result.insertId);
+    return updatedEvent;
 };
